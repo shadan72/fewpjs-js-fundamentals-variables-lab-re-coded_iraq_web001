fe40dca9d8e60fb5b85a04f6f1ccffe052c80a87
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-11 12:24:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
+<<<<<<< HEAD
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
+=======
+let mostProfitableNeighborhood = 'chelsea';
+mostProfitableNeighborhood = 'Upper West Side';
+>>>>>>> 6623de76023d0e5f372a7201b72eedc1a0e437ea
